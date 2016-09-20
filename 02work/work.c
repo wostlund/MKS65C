56abffbc4ab9d9c04bf9a22694dcf5965d555a9d
Will Ostlund <wostlund@stuy.edu>
2016-09-20 10:37:36 -0400

    Added new c file

---
+++ 02work/work.c
@@ -0,0 +1,14 @@
+#include <studio.h>
+#include <stdlib.h>
+
+int x;
+int arr[5];
+
+int main(){
+  printf("%d\n", x);
+  printf("%d\n", arr[0]);
+  printf("%d\n", arr[1]);
+  printf("%d\n", arr[2]);
+  printf("%d\n", arr[3]);
+  printf("%d\n", arr[4]);
+}
